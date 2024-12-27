a9c5b5a933654610b805c8d55241d14faaf51a12
yoshi <suiei1yoshi@gmail.com>
2024-12-27 11:56:34 +0900

    変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Templates from "../components/Index";
+import Index from "../components/Index";
 
 const Home: React.FC = () => {
-  return <Templates/>;
+  return <Index/>;
 }
 export default Home;
