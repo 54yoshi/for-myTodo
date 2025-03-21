fee83da7a99ead4f09b064788edb5a3089dac746
yoshi <suiei1yoshi@gmail.com>
2025-03-21 13:49:40 +0900

    変更

---
+++ src/components/parts/resultSignUp.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ResultSignUp: React.FC = () => {
+  return (
+    <div style={{
+      position: "absolute",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontWeight: "600",
+      fontSize: "40px"
+    }}>ようこそ！！！</div>)
+}
+
+export default ResultSignUp;
