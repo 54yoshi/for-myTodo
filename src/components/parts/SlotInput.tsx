20a3095d52e0d538adcbbcc7628040c10a6d1962
yoshi <suiei1yoshi@gmail.com>
2025-04-01 14:33:47 +0900

    変更

---
+++ src/components/parts/SlotInput.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./SlotRoleInput.module.css";
+
+type Props ={
+  roleInputs: {id: number, src: string, role: string}[],
+  setRoleInputs: React.Dispatch<React.SetStateAction<{id: number, src: string, role: string}[]>>,
+  position: number,
+}
+
+const SlotRoleInput: React.FC<Props> = ({ 
+  roleInputs, 
+  setRoleInputs, 
+  position 
+}) => {
+  function updateRoleInputs(id, newRole){
+    setRoleInputs((prevContents) => {
+      return prevContents.map((prev) => {
+        return prev.id === id ? {...prev, role: newRole} : prev;
+      });
+    });
+  }
+
+  return(
+      <div 
+        className={styles.container}
+        style={{ left: `${position}px`}}
+      >
+        <div className={styles.cover}>
+        </div>
+        {roleInputs.map(({id, src, role}) => (
+          <div 
+            key={id}
+            className={styles.inputContainer}
+          >
+            <img 
+              src={src} 
+              alt="スロットの出目の画像"  
+              className={styles.image}
+            />
+            <div className={styles.textContainer}>
+              <input 
+                type="text" 
+                onChange={e => updateRoleInputs(id, e.target.value)} 
+                value={role}
+                className={styles.text}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+  )
+}
+
+export default SlotRoleInput;
